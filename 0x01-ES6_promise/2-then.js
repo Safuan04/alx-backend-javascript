78c4120e51d8b22c2ed8e032c71957a64f18d7ef
Safuan04 <elmai.safuan@gmail.com>
2024-01-03 23:15:37 +0100

    2. Catch me if you can!

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,14 @@
+export default function handleResponseFromAPI(promise) {
+  const p = new Promise((resolve, reject) => {
+    if (promise) {
+      return resolve({ status: 200, body: 'success' });
+    }
+    return reject(new Error());
+  });
+
+  p.then(() => {
+    console.log('Got a response from the API');
+  }).catch(() => {
+    console.log('Got a response from the API');
+  });
+}
