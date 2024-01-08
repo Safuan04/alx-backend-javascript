6287093ee9f6b5cf49b96aba8a696c7afc9580ec
Safuan04 <elmai.safuan@gmail.com>
2024-01-08 22:13:11 +0100

    0. Basic list of objects

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,21 +1,21 @@
 export default function getListStudents() {
- const firstobj = {
-  id: 1,
-  firstname: 'Guilaume',
-  location: 'San Francisco',
- }
+  const firstobj = {
+    id: 1,
+    firstname: 'Guillaume',
+    location: 'San Francisco',
+  };
 
- const secondtobj = {
-  id: 2,
-  firstname: 'James',
-  location: 'Columbia',
- }
+  const secondtobj = {
+    id: 2,
+    firstname: 'James',
+    location: 'Columbia',
+  };
 
- const thirdtobj = {
-  id: 5,
-  firstname: 'Serena',
-  location: 'San Francisco',
- }
+  const thirdtobj = {
+    id: 5,
+    firstname: 'Serena',
+    location: 'San Francisco',
+  };
 
- return [firstobj, secondtobj, thirdtobj]
+  return [firstobj, secondtobj, thirdtobj];
 }
