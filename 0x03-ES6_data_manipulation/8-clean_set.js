08d9e33d93e0a00103f376dbd0ba420f17be9cf1
Safuan04 <elmai.safuan@gmail.com>
2024-01-09 23:36:41 +0100

    8/10

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,20 @@
 export default function cleanSet(set, startString) {
-  if (set.has(startString)) {
-    return set;
+
+  if (
+    startString.length === 0
+    || typeof startString !== 'string'
+    || typeof set !== 'object'
+    || !(set instanceof Set)
+    || set.length === 0) {
+    return '';
   }
+
+  const setString = [];
+
+  for (const element of set) {
+    if (element && element.startsWith(startString)) {
+      setString.push(element.substring(startString.length));
+    }
+  }
+  return setString.join('-');
 }
